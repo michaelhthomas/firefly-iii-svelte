f715e7de6fb17efc012232740a0f8906f11a2826
Michael Thomas <michaelhthomas@outlook.com>
2023-10-16 12:33:53 -0400

    fix(Icon): make inline prop optional

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -2,7 +2,7 @@
 	import Icon from '@iconify/svelte';
 
 	export let icon: string;
-	export let inline: boolean;
+	export let inline = false;
 </script>
 
 <Icon {icon} {inline} class={inline ? 'inline' : ''} />
