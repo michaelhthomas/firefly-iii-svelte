ef69df84ddfa1c927648df2fde068475d607e04e
Michael Thomas <michaelhthomas@outlook.com>
2023-10-16 12:06:42 -0400

    feat: use iconify for icons

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let icon: string;
+	export let inline: boolean;
+</script>
+
+<Icon {icon} {inline} class={inline ? 'inline' : ''} />
