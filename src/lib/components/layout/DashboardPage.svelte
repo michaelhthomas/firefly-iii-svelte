a436c02d4f89e580fdf1a6839eda87baafce1b64
Michael Thomas <michaelhthomas@outlook.com>
2023-10-17 08:53:04 -0400

    feat: add shared components for creating dashboard pages

---
+++ src/lib/components/layout/DashboardPage.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<div class="container h-full mx-auto py-6 px-5">
+	<div class="space-y-6">
+		<slot name="header">
+			{#if $page.data.meta}
+				<h1 class="h3">
+					<slot name="icon">
+						<Icon icon={$page.data.meta.icon} inline class="pr-1" />
+					</slot>
+					<slot name="title">
+						{$page.data.meta.title}
+					</slot>
+					<slot name="description">
+						{#if $page.data.meta.description}
+							<small class="text-sm font-normal pl-1">
+								{$page.data.meta.description}
+							</small>
+						{/if}
+					</slot>
+				</h1>
+			{/if}
+		</slot>
+
+		<slot />
+	</div>
+</div>
---
+++ src/lib/components/layout/Section.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let title: string | null = null;
+</script>
+
+<div class="card p-4">
+	{#if $$slots.header || $$slots.title || title}
+		<header class="card-header">
+			<slot name="header">
+				<h5 class="h5">
+					<slot name="title">
+						{title}
+					</slot>
+				</h5>
+			</slot>
+		</header>
+	{/if}
+	<section class="p-4 space-y-4">
+		<slot />
+	</section>
+	{#if $$slots.footer}
+		<footer class="card-footer">
+			<slot name="footer" />
+		</footer>
+	{/if}
+</div>
