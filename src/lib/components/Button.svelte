3c5f88276f38a0a3324b7443ff4b46039b66efc1
Michael Thomas <michaelhthomas@outlook.com>
2023-11-07 21:38:51 -0500

    feat: add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Button } from 'flowbite-svelte';
+	import Icon from './Icon.svelte';
+
+	export let icon: string | null = null;
+	export let iconAnchor: 'left' | 'right' = 'left';
+</script>
+
+<Button
+	on:change
+	on:click
+	on:keydown
+	on:keyup
+	on:mouseenter
+	on:mouseleave
+	on:touchcancel
+	on:touchstart
+	on:touchend
+	{...$$restProps}
+>
+	<div class="inline-flex gap-2 items-center">
+		{#if icon && iconAnchor === 'left'}
+			<Icon {icon} class="w-3.5 h-3.5" />
+		{/if}
+
+		<slot />
+
+		{#if icon && iconAnchor === 'right'}
+			<Icon {icon} class="w-c3.5 h-3.5" />
+		{/if}
+	</div>
+</Button>
