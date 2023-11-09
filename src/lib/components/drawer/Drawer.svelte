737ca831af6c618016223f674889d13b907d60e4
Michael Thomas <michaelhthomas@outlook.com>
2023-11-09 10:17:21 -0500

    fix(Drawer): apply global text color

--- src/lib/components/drawer/Drawer.svelte
+++ src/lib/components/drawer/Drawer.svelte
@@ -20,7 +20,7 @@
 	<div
 		transition:fly|global={transitionParams}
 		id="edit-drawer"
-		class="overflow-y-auto z-50 p-4 bg-white dark:bg-gray-800 w-[50rem] max-w-full ma-0 fixed top-0 right-0 h-full space-y-4 flex flex-col"
+		class="overflow-y-auto z-50 p-4 bg-white dark:bg-gray-800 w-[50rem] max-w-full ma-0 fixed top-0 right-0 h-full space-y-4 flex flex-col text-gray-500 dark:text-gray-400"
 	>
 		<div class="flex items-center">
 			<h5
