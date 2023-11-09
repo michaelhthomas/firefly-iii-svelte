b51cd56c24037a1b36123138b9aa8df02e48d869
Michael Thomas <michaelhthomas@outlook.com>
2023-11-08 20:08:30 -0500

    feat: alert component

---
+++ src/lib/components/Alert.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+	import Icon from './Icon.svelte';
+	import type { AlertProps } from 'flowbite-svelte/dist/alert/Alert.svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	export let color: AlertProps['color'] = undefined;
+	export let icon: string | undefined = undefined;
+	export let label: string | undefined = undefined;
+
+	let className: string | undefined = undefined;
+	export { className as class };
+
+	$: darkColorClass = color === 'red' ? 'dark:border-2 dark:border-red-400' : '';
+
+	$: classList = twMerge(darkColorClass, className);
+</script>
+
+<Alert
+	on:blur
+	on:change
+	on:click
+	on:focus
+	on:keydown
+	on:keyup
+	on:mouseenter
+	on:mouseleave
+	{color}
+	class={classList}
+	{...$$restProps}
+>
+	{#if label}
+		<span class="font-semibold">
+			{label}
+		</span>
+	{/if}
+	<slot />
+	<slot name="close-button" slot="close-button" />
+	<slot name="icon" slot="icon">
+		{#if icon}
+			<Icon {icon} class="w-5 h-5" />
+		{/if}
+	</slot>
+</Alert>
