cbb5cdcd1fd9264bca85f3c418464f884cc1579f
Michael Thomas <michaelhthomas@outlook.com>
2023-11-09 10:14:16 -0500

    feat: add details card / grid components

---
+++ src/lib/components/details/DetailCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	let className: string | undefined = undefined;
+	export { className as class };
+
+	export let label: string | undefined = undefined;
+
+	$: mergedClassName = twMerge(
+		'col-span-2 p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 sm:col-span-1 dark:border-gray-600',
+		className
+	);
+</script>
+
+<div class={mergedClassName}>
+	{#if $$slots.label || label}
+		<dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">
+			<slot name="label">
+				{label}
+			</slot>
+		</dt>
+	{/if}
+	<dd class="flex items-center text-gray-500 dark:text-gray-400">
+		<slot />
+	</dd>
+</div>
---
+++ src/lib/components/details/DetailsGrid.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	let className: string | undefined = undefined;
+	export { className as class };
+
+	export let cols = 'grid-cols-2';
+
+	$: mergedClassName = twMerge('grid gap-4 mb-4', cols, className);
+</script>
+
+<dl class={mergedClassName}>
+	<slot />
+</dl>
