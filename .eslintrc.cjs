675b36a1de397965faaa0d91997dce2e9bffd392
Michael Thomas <michaelhthomas@outlook.com>
2023-11-11 20:59:35 -0500

    style: require absolute path imports within $lib folder

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
 		'prettier'
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'unused-imports'],
+	plugins: ['@typescript-eslint', 'unused-imports', 'no-relative-import-paths'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
@@ -38,6 +38,15 @@ module.exports = {
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
+			}
+		},
+		{
+			files: ['src/lib/**/*'],
+			rules: {
+				'no-relative-import-paths/no-relative-import-paths': [
+					'error',
+					{ allowSameFolder: true, rootDir: 'src/lib', prefix: '$lib' }
+				]
 			}
 		}
 	]
