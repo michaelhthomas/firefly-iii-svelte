3865e255e798e7032c724a3b084a38146c00bce7
Michael Thomas <michaelhthomas@outlook.com>
2023-11-13 09:44:36 -0500

    style: fix formatting

--- src/routes/webhooks/+page.svelte
+++ src/routes/webhooks/+page.svelte
@@ -2,10 +2,11 @@
 	import { createQuery } from '@tanstack/svelte-query';
 
 	import { WebhooksApi } from '$lib/api';
+	import { useService } from '$lib/services';
+
 	import DashboardPage from '$lib/components/layout/DashboardPage.svelte';
 	import Section from '$lib/components/layout/Section.svelte';
 	import WebhooksTable from '$lib/components/webhooks/WebhooksTable.svelte';
-	import { useService } from '$lib/services';
 
 	const webhooksService = useService(WebhooksApi);
 
